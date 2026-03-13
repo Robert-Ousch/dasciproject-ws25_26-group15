a6120dffa92dde3922fbd4e065e251442c301111
alexanderliebler <alexander.liebler@gmx.de>
2026-03-13 16:29:48 +0100

    change question8 and add data_q8!

--- pages/Question_8.py
+++ pages/Question_8.py
@@ -6,7 +6,10 @@ import plotly.express as px
 
 
 ### Calculations ###
-
+with open("data_Q8.txt", "r") as file:
+    data_Q8 = json.loads(file.read())
+df8 = pd.DataFrame(data_Q8)
+df8.index = df8.index.astype(int)
 
 ### Page Layout ###
 dash.register_page(__name__)
@@ -14,9 +17,11 @@ dash.register_page(__name__)
 layout = html.Div([
             html.H3('Question 8: How does the venue capacity influence the win rate of the \
                 away team for the seasons 2022-2024?'),
+            html.P("Select upper and lower bounds for the venue capacity:"),
             dcc.RangeSlider(10000, 100000, 1000, value=[10000, 100000], id = 'component8_1'),
+            html.P("Select one or more teams to compare:"),
             dcc.Dropdown(
-                options = ['1. FC Heidenheim 1846', '1. FC Köln', '1. FC Union Berlin', '1. FSV Mainz 05', \
+                options = ['1. FC Heidenheim 1846', '1. FC Köln',	'1. FC Union Berlin', '1. FSV Mainz 05', \
                     'Bayer 04 Leverkusen', 'Borussia Dortmund', 'Borussia Mönchengladbach', \
                     'Eintracht Frankfurt', 'FC Augsburg', 'FC Bayern München', 'FC Schalke 04', 'FC St. Pauli', \
                     'Hertha BSC', 'RB Leipzig', 'SC Freiburg', 'SV Darmstadt 98', 'SV Werder Bremen', \
@@ -37,4 +42,5 @@ def upgrade_graph_8(slider, value_chosen):
     df_temp = pd.DataFrame({})
     rows = df8.loc[(df8.index >= slider[0]) & (df8.index <= slider[1])]
     df_temp = pd.concat([df_temp, rows])
-    return px.scatter(df_temp, y = value_chosen)
+    fig8 = px.scatter(df_temp, y = value_chosen)
+    return fig8

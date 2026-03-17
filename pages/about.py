aebf386396818054f4521d906f033574ac80888f
alexanderliebler <alexander.liebler@gmx.de>
2026-03-17 09:56:16 +0100

    add about us to website

--- pages/about.py
+++ pages/about.py
@@ -5,5 +5,8 @@ dash.register_page(__name__)
 
 layout = html.Div([
     html.H1('About us', style = {'textAlign':'center'}),
-    html.Div('Some text about us:'),
+    html.P('This project is created as part of the Data Science Project of the '\
+        'winter semester 2025/26 from Cristian-Albrechts-University in Kiel. '\
+        'Our group is number 15 and the team members are Hauke Busch, Jakob Erichsen,'\
+        'Alexander Liebler and Robert Ousch.')
 ])

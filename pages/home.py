7c9b9da2fde7e817ef8487132b421f9bc2d206ea
alexanderliebler <alexander.liebler@gmx.de>
2026-03-17 09:53:28 +0100

    add data info to main pate

--- pages/home.py
+++ pages/home.py
@@ -5,5 +5,12 @@ dash.register_page(__name__, path='/')
 
 layout = html.Div([
     html.H1('This is our Home page', style = {'textAlign':'center'}),
-    html.Div('This is our Home page content.'),
+    html.Div('Description of data sources'),
+    html.P('For this project, the main data source is the API api.openligadb.de,' \
+        'which contains historic data of matches, teams and goals.' \
+        'The API v3.football.api-sports.io is used for the venue capacity. '\
+        'Finally, geocoding-api.open-meteo.com incluces weather data, more specifically'\
+        'rainfall and precipitation for given coordinates.'\
+        'The lollipop plot is implemented based on https://hi-artemii.medium.com/vertical-'\
+        'lollipop-chart-in-plotly-python-minimal-code-example-1e1bca0b1261.')
 ])

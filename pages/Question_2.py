b86027921a0f83bb31d66cc468537c097d3b808c
Hauke Busch <stu247182@mail.uni-kiel.de>
2026-03-18 12:56:41 +0100

    add description for Q2 and Q3 to readme (for real this time)

--- pages/Question_2.py
+++ pages/Question_2.py
@@ -1,4 +1,4 @@
-import requests, json
+import json
 import pandas as pd
 import dash
 from dash import html, dcc, callback, Input, Output
@@ -8,15 +8,20 @@ import plotly.express as px
 dash.register_page(__name__)
 
 with open("./.data_Q2.txt", "r") as file:
+    # structured like {team 1: [[goals match 1, ...],
+    # [precipitation match 1, ...]], team 2: ...}
     data_Q2 = json.loads(file.read())
 
 diagrams = []
 
+
 def filter_prec(data, threshold):
+    '''Produce the two box plots from the given data
+    (which is possibly filtered to specific teams)'''
     teams_lt = []
     goals_lt = []
     teams_gt = []
-    goals_gt =[]
+    goals_gt = []
     df1 = pd.DataFrame()
     df2 = pd.DataFrame()
     counts_lt = {}
@@ -43,34 +48,49 @@ def filter_prec(data, threshold):
     df1["Goals"] = goals_lt
     df2["Teams (# Matches)"] = teams_gt
     df2["Goals"] = goals_gt
-    return (px.box(df1, x="Teams (# Matches)", y="Goals", title=f"Goals distribution during games with at most {threshold}mm of precipitation"), 
-            px.box(df2, x="Teams (# Matches)", y="Goals", title=f"Goals distribution during games with more than {threshold}mm of precipitation"))
+    return (px.box(df1, x="Teams (# Matches)", y="Goals",
+    title=f"Goals distribution during games with at most {threshold}mm " \
+      "of precipitation"),
+    px.box(df2, x="Teams (# Matches)", y="Goals",
+    title=f"Goals distribution during games with more than {threshold}mm" \
+      " of precipitation"))
 
 layout = html.Div([
-            html.H3('Question 2: How does high rainfall during a match influence the performance of certain German teams?'),
-            html.P("Many football arenas in Germany do not (yet) have a roof, exposing teams and spectators alike to the elements. " \
-            "We wanted to know if there are any teams out there which are particularly impacted by unfavorable weather conditions. " \
-            "For this purpose, we're identifying a team's performance with the number of goals they scored during the match."),
-            html.P("On this page, you will be able to answer this question for yourself. Move the slider below to see a distribution of scored " \
-            "goals for all selected teams, which only takes into account matches with at most (top plot) or more than (bottom plot) " \
-            "that much precipitation on the day of the match."),
-            html.Div(
-                [html.P("Precipitation threshold (mm):"),
-                dcc.Slider(
-                    id='Q2_rain_filter',
-                    min=0,
-                    max=35,
-                    step=0.1,
-                    value=1.5,
-                )]
-            ),
-            html.Div(
-                [html.P("Selected teams:"),
-                dcc.Dropdown(list(data_Q2.keys()), list(data_Q2.keys()), True, True, True, id="Q2_teams_filter")]
-            ),
-            html.Div(children=dcc.Graph(id= "Q2_0", figure=px.bar())),
-            html.Div(children=dcc.Graph(id= "Q2_1", figure=px.bar())),
-        ], id = 'Q2Div')
+    html.H3("Question 2: How does high rainfall during a match influence" \
+    "the performance of certain German teams?"),
+    html.P("Many football arenas in Germany do not (yet) have a roof, " \
+    "exposing teams and spectators alike to the elements. " \
+    "We wanted to know if there are any teams out there which are " \
+    "particularly impacted by unfavorable weather conditions. " \
+    "For this purpose, we're identifying a team's performance with the " \
+    "number of goals they scored during the match."),
+    html.P("On this page, you will be able to answer this question for " \
+    "yourself. Move the slider below to see a distribution of scored " \
+    "goals for all selected teams, which only takes into account matches" \
+    " with at most (top plot) or more than (bottom plot) " \
+    "that much precipitation on the day of the match."),
+    html.Div(
+        [html.P("Precipitation threshold (mm):"),
+            dcc.Slider(
+                id='Q2_rain_filter',
+                min=0,
+                max=35,
+                step=0.1,
+                value=1.5,
+            )]
+        ),
+    html.Div(
+        [html.P("Selected teams:"),
+        dcc.Dropdown(list(data_Q2.keys()),
+            list(data_Q2.keys()),
+            True,
+            True,
+            True,
+            id="Q2_teams_filter")]
+        ),
+    html.Div(children=dcc.Graph(id= "Q2_0", figure=px.bar())),
+    html.Div(children=dcc.Graph(id= "Q2_1", figure=px.bar())),
+    ], id = 'Q2Div')
 
 @callback(
     Output(component_id="Q2_0", component_property="figure"),
@@ -78,6 +98,7 @@ layout = html.Div([
     Input(component_id="Q2_rain_filter", component_property="value"),
     Input(component_id="Q2_teams_filter", component_property="value")
 )
-def update_plots_Q2(threshold, selection):
+def update_plots_q2(threshold, selection):
+    '''callback handling the user's input'''
     data = dict(filter(lambda x: x[0] in selection, data_Q2.items()))
     return filter_prec(data, threshold)
--- pages/Question_3.py
+++ pages/Question_3.py
@@ -1,7 +1,8 @@
-import requests, json
+import statistics
+import json
 import pandas as pd
 import dash
-import statistics
+
 from dash import html, dcc, callback, Input, Output
 import plotly.express as px
 
@@ -10,19 +11,27 @@ dash.register_page(__name__)
 
 
 with open("./.data_Q3.txt", "r") as file:
+    # structured like {"goals": [goals match 1, ...], "rain":
+    # [rain match 1, ...], "precipitation: [precipitation match 1, ...]"}
     data_Q3 = json.loads(file.read())
 
 df3_r = pd.DataFrame()
 df3_p = pd.DataFrame()
+# average goals, precipitation/rain amounts, and sample sizes for each bucket
 avgs_r = []
 precs_r = []
 ss_r = []
 avgs_p = []
 precs_p = []
-ss_p =[]
+ss_p = []
 for i in range(350):
-    matches_r = list(filter(lambda x: i/10 <= x[1] < i/10+0.1, list(zip(data_Q3["Goals"], data_Q3["Rain"], data_Q3["Precipitation"]))))
-    matches_p = list(filter(lambda x: i/10 <= x[2] < i/10+0.1, list(zip(data_Q3["Goals"], data_Q3["Rain"], data_Q3["Precipitation"]))))
+    # create and analyze the buckets
+    matches_r = list(filter(lambda x: i/10 <= x[1] < i/10 + 0.1,
+        list(zip(data_Q3["Goals"], data_Q3["Rain"],
+            data_Q3["Precipitation"]))))
+    matches_p = list(filter(lambda x: i/10 <= x[2] < i/10 + 0.1,
+        list(zip(data_Q3["Goals"], data_Q3["Rain"],
+            data_Q3["Precipitation"]))))
     if len(matches_r) > 5:
         avgs_r.append(statistics.mean([x for (x, _, _) in matches_r]))
         precs_r.append(i/10)
@@ -35,34 +44,47 @@ for i in range(350):
 df3_r["Average goals per match"] = avgs_r
 df3_r["Rain in mm"] = precs_r
 df3_r["Sample size"] = ss_r
-fig_Q3_r = px.scatter(df3_r, x="Rain in mm", y="Average goals per match", trendline="ols", hover_data="Sample size")
+fig_Q3_r = px.scatter(df3_r, x="Rain in mm", y="Average goals per match",
+    trendline="ols", hover_data="Sample size")
 df3_p["Average goals per match"] = avgs_p
 df3_p["Precipitation in mm"] = precs_p
 df3_p["Sample size"] = ss_p
-fig_Q3_p = px.scatter(df3_p, x="Precipitation in mm", y="Average goals per match", trendline="ols", hover_data="Sample size")
+fig_Q3_p = px.scatter(df3_p, x="Precipitation in mm",
+    y="Average goals per match",
+    trendline="ols",
+    hover_data="Sample size")
 
 layout = [
-        html.Div([
-            html.H3('Question 3: How does high precipitation during a match influence the total number of goals scored?'),
-            html.P("Taking a step back from the previous question, on this page we're interested in a general trend in the amount of goals scored " \
-            "during the average football match as the precipitation changes. We therefore group matches into buckets based on precipitation " \
-            "with a bucket size of 0.1mm, then plot the average number of goals for each bucket below. This naturally creates an imbalance " \
-            "in the bucket sizes, as about half of all games see no precipitation at all whereas only about a quarter of games see a precipitation " \
-            "greater than 3mm. This is the reason for the increase in variance we observe in the plot, not the precipitation itself. Only buckets with " \
-            "a size of 6 or greater are considered to eliminate outliers. As we can see, the observed trend varies little when considering rain specifically " \
-            "as opposed to precipitation, which also includes all other ways water can fall from the sky."),
-            html.Div(dcc.RadioItems(["Rain", "Precipitation"], "Precipitation", id="Q3_rain_select")),
-            html.Div(children=dcc.Graph(id= "Q3_1", figure=px.scatter())),
-        ], id = 'Q3Div')
-        ]
+    html.Div([
+        html.H3("Question 3: How does high precipitation during a match " \
+                "influence the total number of goals scored?"),
+        html.P("Taking a step back from the previous question, on this page" \
+        " we're interested in a general trend in the amount of goals scored" \
+        " during the average football match as the precipitation changes." \
+        " We therefore group matches into buckets based on precipitation " \
+        "with a bucket size of 0.1mm, then plot the average number of goals" \
+        " for each bucket below. This naturally creates an imbalance in the" \
+        " bucket sizes, as about half of all games see no precipitation at " \
+        "all whereas only about a quarter of games see a precipitation " \
+        "greater than 3mm. This is the reason for the increase in variance" \
+        " we observe in the plot, not the precipitation itself. Only buckets"\
+        " with a size of 6 or greater are considered to eliminate outliers. "\
+        "As we can see, the observed trend varies little when considering " \
+        "rain specifically as opposed to precipitation, which also includes" \
+        " all other ways water can fall from the sky."),
+        html.Div(dcc.RadioItems(["Rain", "Precipitation"], "Precipitation",
+            id="Q3_rain_select")),
+        html.Div(children=dcc.Graph(id= "Q3_1", figure=px.scatter())),
+    ], id = 'Q3Div')
+    ]
 
 
 @callback(
         Output(component_id="Q3_1", component_property="figure"),
         Input(component_id="Q3_rain_select", component_property="value")
 )
-def Q3_graph_switch(selection):
+def q3_graph_switch(selection):
+    '''callback to allow the user to toggle between the graphs'''
     if selection=="Rain":
         return fig_Q3_r
-    else:
-        return fig_Q3_p
+    return fig_Q3_p

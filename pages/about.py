34a4e4d2619ad945f3fb5804839ea9550021ea2f
alexanderliebler <alexander.liebler@gmx.de>
2026-03-18 15:03:15 +0100

    add whitespace

--- pages/about.py
+++ pages/about.py
@@ -7,6 +7,6 @@ layout = html.Div([
     html.H1('About us', style = {'textAlign':'center'}),
     html.P('This project is created as part of the Data Science Project of the '\
         'winter semester 2025/26 from Cristian-Albrechts-University in Kiel. '\
-        'Our group is number 15 and the team members are Hauke Busch, Jakob Erichsen,'\
+        'Our group is number 15 and the team members are Hauke Busch, Jakob Erichsen, '\
         'Alexander Liebler and Robert Ousch.')
 ])
